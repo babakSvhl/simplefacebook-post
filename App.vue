ccb689f7b0100cdd258ec730574f25699f99514e
babakSvhl <52063224+babakSvhl@users.noreply.github.com>
2022-04-05 09:08:44 -0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app" class="container">
+    
+    
+    
+    <div class="container">
+    <HelloWorld/>
+    </div>
+    
+    
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      seen:true,
+      
+      getMenuItems:{
+                1: {
+  'name': 'Margherita',
+  'description': 'A delicious tomato based pizza topped with mozzarella',
+  'options': [{
+    'size': 9,
+    'price': 6.95
+  }, {
+    'size': 12,
+    'price': 10.95
+  }]
+},
+2: {
+  'name': 'Pepperoni',
+  'description': 'A delicious tomato based pizza topped with mozzarella and pepperoni',
+  'options': [{
+    'size': 9,
+    'price': 7.95
+  }, {
+    'size': 12,
+    'price': 12.95
+  }]
+},
+3: {
+  'name': 'Ham and Pineapple',
+  'description': 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+  'options': [{
+    'size': 9,
+    'price': 7.95
+  }, {
+    'size': 12,
+    'price': 12.95
+  }]
+
+}
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  
+      methods: {
+        
+        
+          
+        
+      },
+    
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.scrollable{
+  overflow-y: scroll;
+}
+.scrollv{
+  height: 20px;
+}
+.scroll{
+  height: 400px;
+}
+
+.left{
+  float: left;
+}
+.right{
+  float: right;
+}
+</style>
---
+++ HelloWorld.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="card">
+    
+    <div class="container row">
+      <div class="card header">
+          <h2>whats in your mind</h2>
+      </div>
+      <div class="card body">
+         <input class="form-control" 
+         placeholder="Write Here..." v-model="txtitem"/>
+
+      <div class="btnend">
+      <button class="btn btn-primary btnend glow"
+      @click="addpost" type="button">Post</button>
+      </div>
+      
+         
+      </div>
+    </div>
+   <div class="center">
+      <div class="card cardsize" v-for="item in post" :key="item.id">
+        <div class="card-header">
+          <h4 class="starth">babak</h4>
+        </div>
+        <div class="card-body bodyform">
+           <textarea v-model="item.name"></textarea>
+        </div>
+        <div>
+          <button class="btn btn-outline-primary btnstart">Like</button>
+        </div>
+
+      </div>
+   </div>
+   
+   <div class="ah">
+   <transition name="alert">
+     <div v-if="seen" class="alert-success" >
+     <button class="close" type="button" @click="seen=false"></button><h4> با موفقیت پست شد </h4></div>
+  </transition>
+   </div>
+   
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      post:[],
+      txtitem:null,
+      seen:false
+    }
+  },
+
+  
+
+  methods: {
+    addpost(){
+      
+      this.post.push({
+        name:this.txtitem,
+        
+      }),
+      this.seen=true
+      console.log(this.post)
+    },
+    
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+
+}
+
+.btnend{
+  position:relative;
+  left: 20%;
+}
+
+.starth{
+  position: relative;
+  right: 20%;
+}
+
+.fntsize{
+     font-size: 32pt;
+}
+
+.cardsize{
+  max-width: 500px;
+  max-height: 150%;
+  
+}
+.center {
+margin: auto;
+width: 60%;
+padding: 10px;
+}
+
+.btnstart{
+  position: relative;
+  right: 20%;
+}
+
+.glow:hover{
+animation: glowing 1s infinite;
+}
+
+
+@keyframes glowing{
+  0%{
+    box-shadow: 0 0 2px #2828ff;
+  }
+  50%{
+    box-shadow: 0 0 15px #2828ff;
+  }
+  100%{
+    box-shadow: 0 0 2px #2828ff;
+  }
+}
+
+.alert-enter-active{
+animation-duration: 1s;
+animation-fill-mode: both;
+animation-name: slidein;
+}
+.alert-leave-active{
+animation-duration: 1s;
+animation-fill-mode: both;
+animation-name: slideout;
+}
+@keyframes slideout{
+  from{
+    transform: translate3d(0,0,0);
+  }
+  to{
+    visibility: hidden;
+    transform: translate3d(100%,0,0);
+  }
+}
+@keyframes slidein{
+  from{
+    transform: translate3d(100%,0,0);
+    visibility: visible;
+  }
+  to{
+    transform: translate3d(0,0,0);
+  }
+}
+.ah{
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+</style>
